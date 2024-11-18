885887d9e5e594bb1e501e75f5abbdfbdd49be74
Henri Fardeau <fardeauhenri@gmail.com>
2024-11-18 13:02:07 +0100

    fix: page padding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
     <html lang="fr">
       <body>
         <header className="fixed inset-x-0 top-0 z-40 h-16 border-b border-[#252525] bg-[#111111]" />
-        <main>{children}</main>
+        <main className="pt-16">{children}</main>
       </body>
     </html>
   );
